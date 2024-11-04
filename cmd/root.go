3ace89458c98506f650cc1ed132251df466fff1c
umutteroll <umut22erol@gmail.com>
2024-11-04 10:32:58 +0000

    fix : deb package and delete command

--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
-    "os"
-    "github.com/spf13/cobra"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-    Use:   "codegen",
-    Short: "useful application for creating folders and classes",
-    Long:  `This app creates service, entities, controller, route file while user creates a new folder`,
+	Use:   "codegen",
+	Short: "useful application for creating folders and classes",
+	Long:  `This app creates service, entities, controller, route file while user creates a new folder`,
 }
 
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        os.Exit(1)
-    }
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
 
 func init() {
 
-    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.codegen.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.codegen.yaml)")
 }
--- cmd/run.go
+++ cmd/run.go
@@ -1,15 +1,17 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/KadirOzerOzturk/deneme/helpers"
 	"github.com/spf13/cobra"
 )
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
-	Use:   "run [name]",
-	Short: "Print a greeting message",
-	Long:  `This command prints a greeting message with the provided name.`,
+	Use:   "create [name]",
+	Short: "create",
+	Long:  `This func use for create new files`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		name := args[0]
@@ -18,7 +20,20 @@ var runCmd = &cobra.Command{
 	},
 }
 
+// runCmd represents the run command
+var deleteCmd = &cobra.Command{
+	Use:   "delete [name]",
+	Short: "delete",
+	Long:  `This func use for delete created files`,
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+
+		fmt.Println("delete template : " + name)
+	},
+}
 func init() {
 
 	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(deleteCmd)
 }
